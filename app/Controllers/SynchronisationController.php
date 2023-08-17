1a180d51d0ccdbb7212780b50306d8114da61323
elektr0nisch <contact@elektronisch.dev>
2023-08-17 23:19:11 +0200

    Finish ldap sync

--- app/Controllers/SynchronisationController.php
+++ app/Controllers/SynchronisationController.php
@@ -12,8 +12,11 @@ use LdapRecord\LdapRecordException;
 use LdapRecord\Models\Model;
 use LdapRecord\Models\OpenLDAP\User;
 use LdapRecord\Query\Model\Builder;
+use function App\Helpers\getGroupById;
 use function App\Helpers\getGroups;
+use function App\Helpers\getSchoolById;
 use function App\Helpers\getSchools;
+use function App\Helpers\getUserByUsername;
 use function App\Helpers\getUserByUsernameAndPassword;
 use function App\Helpers\getUsers;
 use function App\Helpers\login;
@@ -30,9 +33,11 @@ class SynchronisationController extends BaseController
             CLI::write('Synchronizing users with LDAP ...');
             $this->syncUsersLDAP();
 
+            CLI::write(' ');
             CLI::write('Synchronizing groups with LDAP ...');
             $this->syncGroupsLDAP();
 
+            CLI::write(' ');
             CLI::write('Synchronizing schools with LDAP ...');
             $this->syncSchoolsLDAP();
 
@@ -55,7 +60,13 @@ class SynchronisationController extends BaseController
             }
         }
 
-        // TODO Remove old users
+        $ldapUsers = User::query()->in(getenv('ldap.usersDN'))->paginate();
+        foreach ($ldapUsers as $ldapUser) {
+            if (is_null(getUserByUsername($ldapUser->uid[0]))) {
+                $ldapUser->delete();
+                CLI::write('Deleted user ' . $ldapUser->uid[0]);
+            }
+        }
     }
 
     /**
@@ -102,7 +113,13 @@ class SynchronisationController extends BaseController
             }
         }
 
-        // TODO Remove old groups
+        $ldapGroups = CustomGroup::query()->in(getenv('ldap.groupsDN'))->paginate();
+        foreach ($ldapGroups as $ldapGroup) {
+            if (is_null(getGroupById($ldapGroup->uid[0]))) {
+                $ldapGroup->delete();
+                CLI::write('Deleted group ' . $ldapGroup->cn[0]);
+            }
+        }
     }
 
     /**
@@ -128,15 +145,33 @@ class SynchronisationController extends BaseController
 
         $ldapGroup->cn = $group->getName();
 
+        // Remove former users from the ldap group
+        // TODO possible improvements: don't query each users' username
         foreach ($ldapGroup->members()->get() as $member) {
+            $isMember = false;
             foreach ($memberships as $membership) {
-                if($member->uid[0] == $membership->)
+                if ($member->uid[0] == $membership->getUser()->getUsername()) {
+                    $isMember = true;
+                    break;
+                }
+            }
+
+            // Remove user from group
+            if (!$isMember) {
+                $ldapGroup->members()->detach($member);
+                CLI::write('Removed ' . $member->uid[0] . ' from group ' . $group->getName());
             }
         }
 
+        // Add new users to the group
+        // TODO possible improvements: don't query each users' username
         foreach ($memberships as $membership) {
-            $ldapUser = $this->getLDAPUserByUsername($membership->getUser()->getUsername());
-            $ldapGroup->members()->attach($ldapUser);
+            $user = $membership->getUser();
+            $ldapUser = $this->getLDAPUserByUsername($user->getUsername());
+            if (!$ldapGroup->members()->contains($ldapUser)) {
+                $ldapGroup->members()->attach($ldapUser);
+                CLI::write('Added ' . $user->getUsername() . ' to group ' . $group->getName());
+            }
         }
 
         $ldapGroup->save();
@@ -150,6 +185,14 @@ class SynchronisationController extends BaseController
                 $this->updateOrCreateLDAPSchool($school);
             } catch (LdapRecordException $e) {
                 CLI::error('Error synchronizing school ' . $school->getName() . ': ' . $e->getMessage());
+            }
+        }
+
+        $ldapGroups = CustomGroup::query()->in(getenv('ldap.schoolsDN'))->paginate();
+        foreach ($ldapGroups as $ldapGroup) {
+            if (is_null(getSchoolById($ldapGroup->uid[0]))) {
+                $ldapGroup->delete();
+                CLI::write('Deleted school ' . $ldapGroup->cn[0]);
             }
         }
     }
@@ -178,16 +221,29 @@ class SynchronisationController extends BaseController
         $ldapGroup->cn = $school->getName();
 
         foreach ($ldapGroup->members()->get() as $member) {
-            CLI::write($member->uid[0]);
+            $isMember = false;
+            foreach ($students as $student) {
+                if ($member->uid[0] == $student->getUsername()) {
+                    $isMember = true;
+                    break;
+                }
+            }
+
+            // Remove user from group
+            if (!$isMember) {
+                $ldapGroup->members()->detach($member);
+                CLI::write('Removed ' . $member->uid[0] . ' from school ' . $school->getName());
+            }
         }
 
         foreach ($students as $user) {
             $ldapUser = $this->getLDAPUserByUsername($user->getUsername());
-            $ldapGroup->members()->attach($ldapUser);
+            if (!$ldapGroup->members()->contains($ldapUser)) {
+                $ldapGroup->members()->attach($ldapUser);
+                CLI::write('Added ' . $user->getUsername() . ' to school ' . $school->getName());
+            }
         }
 
-        // TODO add new members
-        // TODO remove old members
         $ldapGroup->save();
         CLI::write('Successfully synced school ' . $school->getName());
     }
