cdc614d9376a67ccac331899b21642f86465af8e
elektr0nisch <contact@elektronisch.dev>
2023-11-19 21:26:54 +0100

    Improve group und school views

--- app/Views/group/GroupView.php
+++ app/Views/group/GroupView.php
@@ -42,139 +42,169 @@ $ownMembership = getGroupMembership($currentUser->getId(), $group->getId());
     </div>
 <?php endif; ?>
 
-<h3 class="subheader">Allgemeines</h3>
 <div class="row">
-    <div class="col-md-6">
-        <table>
-            <tr>
-                <?php
-                $groupLogoPath = "/assets/img/group/" . $group->getId() . "/logo.png";
-                if (is_file($_SERVER['DOCUMENT_ROOT'] . $groupLogoPath)) {
-                    $groupLogoSrc = base_url($groupLogoPath);
-                } else {
-                    $groupLogoSrc = base_url('/assets/img/placeholders/group-logo_512x128.png');
-                }
-                ?>
-                <img class="img-thumbnail mb-3" src="<?= $groupLogoSrc ?>"
-                     style="max-width: 100%; width: 512px; height: auto"
-                     alt="Logo <?= $group->getName() ?>">
-            </tr>
-            <tr>
-                <th>Name:&nbsp;</th>
-                <td><?= $group->getName() ?></td>
-            </tr>
-            <tr>
-                <th>Beschreibung:&nbsp;</th>
-                <td><?= $group->getDescription() ?></td>
-            </tr>
-        </table>
+    <div class="col-lg-8">
+        <div class="card mb-4">
+            <div class="card-header">Informationen</div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <table>
+                            <tr>
+                                <?php
+                                $groupLogoPath = "/assets/img/group/" . $group->getId() . "/logo.png";
+                                if (is_file($_SERVER['DOCUMENT_ROOT'] . $groupLogoPath)) {
+                                    $groupLogoSrc = base_url($groupLogoPath);
+                                } else {
+                                    $groupLogoSrc = base_url('/assets/img/placeholders/group-logo_512x128.png');
+                                }
+                                ?>
+                                <img class="img-thumbnail mb-3" src="<?= $groupLogoSrc ?>"
+                                     alt="Logo <?= $group->getName() ?>">
+                            </tr>
+                            <tr>
+                                <th>Name:&nbsp;</th>
+                                <td><?= $group->getName() ?></td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div class="col-lg-6">
+                        <figure>
+                            <?php
+                            $groupImagePath = "/assets/img/group/" . $group->getId() . "/image.png";
+                            if (is_file($_SERVER['DOCUMENT_ROOT'] . $groupImagePath)) {
+                                $groupImageSrc = base_url($groupImagePath);
+                            } else {
+                                $groupImageSrc = base_url('/assets/img/placeholders/group-image_1920x1080.png');
+                            }
+                            ?>
+                            <a href="<?= $groupImageSrc ?>" data-toggle="lightbox">
+                                <img class="img-thumbnail mt-3" src="<?= $groupImageSrc ?>"
+                                     alt="Logo <?= $group->getName() ?>">
+                            </a>
+                            <figcaption>
+                                <small><?= !is_null($group->getImageAuthor()) ? '&copy;&nbsp;' . $group->getImageAuthor() : '' ?></small>
+                            </figcaption>
+                        </figure>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <?= $group->getDescription() ?>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="col-md-6">
-        <figure>
-            <?php
-            $groupImagePath = "/assets/img/group/" . $group->getId() . "/image.png";
-            if (is_file($_SERVER['DOCUMENT_ROOT'] . $groupImagePath)) {
-                $groupImageSrc = base_url($groupImagePath);
-            } else {
-                $groupImageSrc = base_url('/assets/img/placeholders/group-image_1920x1080.png');
-            }
-            ?>
-            <a href="<?= $groupImageSrc ?>" data-toggle="lightbox">
-                <img class="img-thumbnail mt-3" src="<?= $groupImageSrc ?>"
-                     style="max-width: 100%; width: auto; height: auto; border-radius: 10px;"
-                     alt="Logo <?= $group->getName() ?>">
-            </a>
-            <figcaption>
-                <small><?= !is_null($group->getImageAuthor()) ? '&copy;&nbsp;' . $group->getImageAuthor() : '' ?></small>
-            </figcaption>
-        </figure>
+    <div class="col-lg-4">
+        <div class="card">
+            <div class="card-header">Aktionen</div>
+            <div class="card-body">
+                <?= form_open('group/join', ['onsubmit' => "return confirm('Möchtest du der Gruppe {$group->getName()} wirklich beitreten?');"]) ?>
+                <?= form_hidden('id', $group->getId()) ?>
+                <button type="submit" class="btn btn-success">
+                    <i class="fas fa-sign-in"></i> Beitrittsanfrage senden
+                </button>
+                <?= form_close() ?>
+            </div>
+        </div>
     </div>
 </div>
-<h3 class="subheader">Nutzer*innen</h3>
 
-<table class="table table-striped table-bordered" data-locale="<?= service('request')->getLocale(); ?>"
-       data-toggle="table" data-search="true" data-height="500" data-pagination="true"
-       data-show-columns="true" data-cookie="true" data-cookie-id-table="user"
-       data-search-highlight="true" data-show-columns-toggle-all="true">
-    <thead>
-    <tr>
-        <th data-field="name" data-sortable="true" scope="col">Vor- und Nachname</th>
-        <th data-field="userRole" data-sortable="true" scope="col">Nutzerrolle</th>
-        <th data-field="groupRole" data-sortable="true" scope="col">Gruppenrolle</th>
-        <?php if ($group->mayManage($currentUser)): ?>
-            <th data-field="actions" data-sortable="true" scope="col">Aktionen</th>
-        <?php endif; ?>
-    </tr>
-    </thead>
-    <tbody>
-    <?php foreach (getGroupMembershipsByGroupId($group->getId()) as $membership): ?>
-        <?php if ($membership->getStatus() == MembershipStatus::PENDING): ?>
-            <?php if (!$group->mayManage($currentUser)): continue; endif; ?>
-
-            <tr>
-                <td id="td-id-<?= ($user = $membership->getUser())->getId() ?>"
-                    class="td-class-<?= $user->getId() ?>"
-                    data-title="<?= $user->getName() ?>"><?= $user->getName() ?></td>
-                <td><?= $user->getRole()->badge() ?></td>
-                <td><?= $membership->getStatus()->badge() ?></td>
-                <td>
-                    <?= form_open('group/accept') ?>
-                    <?= form_hidden('groupId', $group->getId()) ?>
-                    <?= form_hidden('userId', $user->getId()) ?>
-                    <button type="submit" class="btn btn-success btn-sm">
-                        <i class="fas fa-check-circle"></i> Akzeptieren
-                    </button>
-                    <?= form_close() ?>
-
-                    <?= form_open('group/deny') ?>
-                    <?= form_hidden('groupId', $group->getId()) ?>
-                    <?= form_hidden('userId', $user->getId()) ?>
-                    <button type="submit" class="btn btn-danger btn-sm mt-1">
-                        <i class="fas fa-x"></i> Ablehnen
-                    </button>
-                    <?= form_close() ?>
-                </td>
-            </tr>
-        <?php else: ?>
-            <tr>
-                <td id="td-id-<?= ($user = $membership->getUser())->getId() ?>"
-                    class="td-class-<?= $user->getId() ?>"
-                    data-title="<?= $user->getName() ?>"><?= $user->getName() ?></td>
-                <td><?= $user->getRole()->badge() ?></td>
-                <td><?= $membership->getStatus()->badge() ?></td>
-                <?php if ($group->mayManage($currentUser)): ?>
-                    <td>
-                        <?php if ($membership->getStatus() == MembershipStatus::ADMIN): ?>
-                            <?= form_open('group/change_user_status') ?>
-                            <?= form_hidden('groupId', $group->getId()) ?>
-                            <?= form_hidden('userId', $user->getId()) ?>
-                            <?= form_hidden('status', MembershipStatus::USER->value) ?>
-                            <button type="submit" class="btn btn-primary btn-sm mt-1">
-                                <i class="fas fa-arrow-down"></i> Zurückstufen
-                            </button>
-                            <?= form_close() ?>
-                        <?php else: ?>
-                            <?= form_open('group/change_user_status') ?>
-                            <?= form_hidden('groupId', $group->getId()) ?>
-                            <?= form_hidden('userId', $user->getId()) ?>
-                            <?= form_hidden('status', MembershipStatus::ADMIN->value) ?>
-                            <button type="submit" class="btn btn-danger btn-sm mt-1">
-                                <i class="fas fa-arrow-up"></i> Zu Admin ernennen
-                            </button>
-                            <?= form_close() ?>
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card">
+            <div class="card-header">Nutzer*innen</div>
+            <div class="card-body">
+                <table class="table table-striped table-bordered" data-locale="<?= service('request')->getLocale(); ?>"
+                       data-toggle="table" data-search="true" data-height="800" data-pagination="true"
+                       data-show-columns="true" data-cookie="true" data-cookie-id-table="user"
+                       data-search-highlight="true" data-show-columns-toggle-all="true">
+                    <thead>
+                    <tr>
+                        <th data-field="name" data-sortable="true" scope="col">Vor- und Nachname</th>
+                        <th data-field="userRole" data-sortable="true" scope="col">Nutzerrolle</th>
+                        <th data-field="groupRole" data-sortable="true" scope="col">Gruppenrolle</th>
+                        <?php if ($group->mayManage($currentUser)): ?>
+                            <th data-field="actions" data-sortable="true" scope="col">Aktionen</th>
                         <?php endif; ?>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php foreach (getGroupMembershipsByGroupId($group->getId()) as $membership): ?>
+                        <?php if ($membership->getStatus() == MembershipStatus::PENDING): ?>
+                            <?php if (!$group->mayManage($currentUser)): continue; endif; ?>
 
-                        <?= form_open('group/kick_user') ?>
-                        <?= form_hidden('groupId', $group->getId()) ?>
-                        <?= form_hidden('userId', $user->getId()) ?>
-                        <button type="submit" class="btn btn-danger btn-sm mt-1">
-                            <i class="fas fa-trash"></i> Entfernen
-                        </button>
-                        <?= form_close() ?>
-                    </td>
-                <?php endif; ?>
-            </tr>
-        <?php endif; ?>
-    <?php endforeach; ?>
-    </tbody>
-</table>
+                            <tr>
+                                <td id="td-id-<?= ($user = $membership->getUser())->getId() ?>"
+                                    class="td-class-<?= $user->getId() ?>"
+                                    data-title="<?= $user->getName() ?>"><?= $user->getName() ?></td>
+                                <td><?= $user->getRole()->badge() ?></td>
+                                <td><?= $membership->getStatus()->badge() ?></td>
+                                <td>
+                                    <?= form_open('group/accept') ?>
+                                    <?= form_hidden('groupId', $group->getId()) ?>
+                                    <?= form_hidden('userId', $user->getId()) ?>
+                                    <button type="submit" class="btn btn-success btn-sm">
+                                        <i class="fas fa-check-circle"></i> Akzeptieren
+                                    </button>
+                                    <?= form_close() ?>
+
+                                    <?= form_open('group/deny') ?>
+                                    <?= form_hidden('groupId', $group->getId()) ?>
+                                    <?= form_hidden('userId', $user->getId()) ?>
+                                    <button type="submit" class="btn btn-danger btn-sm mt-1">
+                                        <i class="fas fa-x"></i> Ablehnen
+                                    </button>
+                                    <?= form_close() ?>
+                                </td>
+                            </tr>
+                        <?php else: ?>
+                            <tr>
+                                <td id="td-id-<?= ($user = $membership->getUser())->getId() ?>"
+                                    class="td-class-<?= $user->getId() ?>"
+                                    data-title="<?= $user->getName() ?>"><?= $user->getName() ?></td>
+                                <td><?= $user->getRole()->badge() ?></td>
+                                <td><?= $membership->getStatus()->badge() ?></td>
+                                <?php if ($group->mayManage($currentUser)): ?>
+                                    <td>
+                                        <?php if ($membership->getStatus() == MembershipStatus::ADMIN): ?>
+                                            <?= form_open('group/change_user_status') ?>
+                                            <?= form_hidden('groupId', $group->getId()) ?>
+                                            <?= form_hidden('userId', $user->getId()) ?>
+                                            <?= form_hidden('status', MembershipStatus::USER->value) ?>
+                                            <button type="submit" class="btn btn-primary btn-sm mt-1">
+                                                <i class="fas fa-arrow-down"></i> Zurückstufen
+                                            </button>
+                                            <?= form_close() ?>
+                                        <?php else: ?>
+                                            <?= form_open('group/change_user_status') ?>
+                                            <?= form_hidden('groupId', $group->getId()) ?>
+                                            <?= form_hidden('userId', $user->getId()) ?>
+                                            <?= form_hidden('status', MembershipStatus::ADMIN->value) ?>
+                                            <button type="submit" class="btn btn-danger btn-sm mt-1">
+                                                <i class="fas fa-arrow-up"></i> Zu Admin ernennen
+                                            </button>
+                                            <?= form_close() ?>
+                                        <?php endif; ?>
+
+                                        <?= form_open('group/kick_user') ?>
+                                        <?= form_hidden('groupId', $group->getId()) ?>
+                                        <?= form_hidden('userId', $user->getId()) ?>
+                                        <button type="submit" class="btn btn-danger btn-sm mt-1">
+                                            <i class="fas fa-trash"></i> Entfernen
+                                        </button>
+                                        <?= form_close() ?>
+                                    </td>
+                                <?php endif; ?>
+                            </tr>
+                        <?php endif; ?>
+                    <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
--- app/Views/school/SchoolView.php
+++ app/Views/school/SchoolView.php
@@ -15,95 +15,115 @@ use function App\Helpers\getUsersBySchoolId;
 
 <h1 class="header"><?= $school->getName() ?></h1>
 
-<h3 class="subheader">Allgemeines</h3>
 <div class="row">
-    <div class="col-md-6">
-        <table>
-            <tr>
-                <?php
-                $schoolLogoPath = "/assets/img/school/" . $school->getId() . "/logo.png";
-                if (is_file($_SERVER['DOCUMENT_ROOT'] . $schoolLogoPath)) {
-                    $schoolLogoSrc = base_url($schoolLogoPath);
-                } else {
-                    $schoolLogoSrc = base_url('/assets/img/placeholders/school-logo_512x128.png');
-                }
-                ?>
-                <img class="img-thumbnail mb-3" src="<?= $schoolLogoSrc ?>"
-                     style="max-width: 100%; width: 512px; height: auto"
-                     alt="Logo <?= $school->getName() ?>">
-            </tr>
-            <tr>
-                <th>Schulname:&nbsp;</th>
-                <td><?= $school->getName() ?></td>
-            </tr>
-            <tr>
-                <th>Kurzname:&nbsp;</th>
-                <td><?= $school->getShortName() ?></td>
-            </tr>
-            <tr>
-                <th>Adresse:&nbsp;</th>
-                <td><?= $school->getAddress() ?></td>
-            </tr>
-            <tr>
-                <th>E-Mail (Verwaltung):&nbsp;</th>
-                <td>
-                    <a href="mailto:<?= $school->getEmailBureau() ?>"><?= $school->getEmailBureau() ?></a>
-                </td>
-            </tr>
-            <tr>
-                <th>E-Mail (SMV):&nbsp;</th>
-                <td>
-                    <?php if (is_null($school->getEmailSMV())): ?>
-                        <i>(nicht definiert)</i>
-                    <?php else: ?>
-                        <a href="mailto:<?= $school->getEmailSMV() ?>"><?= $school->getEmailSMV() ?></a>
-                    <?php endif; ?>
-                </td>
-            </tr>
-        </table>
+    <div class="col-lg-8">
+        <div class="card mb-4">
+            <div class="card-header">Informationen</div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <table>
+                            <tr>
+                                <?php
+                                $schoolLogoPath = "/assets/img/school/" . $school->getId() . "/logo.png";
+                                if (is_file($_SERVER['DOCUMENT_ROOT'] . $schoolLogoPath)) {
+                                    $schoolLogoSrc = base_url($schoolLogoPath);
+                                } else {
+                                    $schoolLogoSrc = base_url('/assets/img/placeholders/school-logo_512x128.png');
+                                }
+                                ?>
+                                <img class="img-thumbnail mb-3" src="<?= $schoolLogoSrc ?>"
+                                     alt="Logo <?= $school->getName() ?>">
+                            </tr>
+                            <tr>
+                                <th>Schulname:&nbsp;</th>
+                                <td><?= $school->getName() ?></td>
+                            </tr>
+                            <tr>
+                                <th>Kurzname:&nbsp;</th>
+                                <td><?= $school->getShortName() ?></td>
+                            </tr>
+                            <tr>
+                                <th>Adresse:&nbsp;</th>
+                                <td><?= $school->getAddress() ?></td>
+                            </tr>
+                            <tr>
+                                <th>E-Mail (Verwaltung):&nbsp;</th>
+                                <td>
+                                    <a href="mailto:<?= $school->getEmailBureau() ?>"><?= $school->getEmailBureau() ?></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>E-Mail (SMV):&nbsp;</th>
+                                <td>
+                                    <?php if (is_null($school->getEmailSMV())): ?>
+                                        <i>(nicht definiert)</i>
+                                    <?php else: ?>
+                                        <a href="mailto:<?= $school->getEmailSMV() ?>"><?= $school->getEmailSMV() ?></a>
+                                    <?php endif; ?>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div class="col-lg-6">
+                        <figure>
+                            <?php
+                            $schoolImagePath = "/assets/img/school/" . $school->getId() . "/image.png";
+                            if (is_file($_SERVER['DOCUMENT_ROOT'] . $schoolImagePath)) {
+                                $schoolImageSrc = base_url($schoolImagePath);
+                            } else {
+                                $schoolImageSrc = base_url('/assets/img/placeholders/school-image_1920x1080.png');
+                            }
+                            ?>
+                            <a href="<?= $schoolImageSrc ?>" data-toggle="lightbox">
+                                <img class="img-thumbnail mt-3" src="<?= $schoolImageSrc ?>"
+                                     alt="Logo <?= $school->getName() ?>">
+                            </a>
+                            <figcaption>
+                                <small><?= !is_null($school->getImageAuthor()) ? '&copy;&nbsp;' . $school->getImageAuthor() : '' ?></small>
+                            </figcaption>
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="col-md-6">
-        <figure>
-            <?php
-            $schoolImagePath = "/assets/img/school/" . $school->getId() . "/image.png";
-            if (is_file($_SERVER['DOCUMENT_ROOT'] . $schoolImagePath)) {
-                $schoolImageSrc = base_url($schoolImagePath);
-            } else {
-                $schoolImageSrc = base_url('/assets/img/placeholders/school-image_1920x1080.png');
-            }
-            ?>
-            <a href="<?= $schoolImageSrc ?>" data-toggle="lightbox">
-                <img class="img-thumbnail mt-3" src="<?= $schoolImageSrc ?>"
-                     style="max-width: 100%; width: auto; height: auto; border-radius: 10px;"
-                     alt="Logo <?= $school->getName() ?>">
-            </a>
-            <figcaption>
-                <small><?= !is_null($school->getImageAuthor()) ? '&copy;&nbsp;' . $school->getImageAuthor() : '' ?></small>
-            </figcaption>
-        </figure>
+    <div class="col-lg-4">
+        <div class="card">
+            <div class="card-header">Aktionen</div>
+            <div class="card-body text-center">
+            </div>
+        </div>
     </div>
 </div>
 
-<h3 class="subheader">Nutzer*innen</h3>
-
-<table class="table table-striped table-bordered" data-locale="<?= service('request')->getLocale(); ?>"
-       data-toggle="table" data-search="true" data-height="500" data-pagination="true"
-       data-show-columns="true" data-cookie="true" data-cookie-id-table="user"
-       data-search-highlight="true" data-show-columns-toggle-all="true">
-    <thead>
-    <tr>
-        <th data-field="name" data-sortable="true" scope="col">Vor- und Nachname</th>
-        <th data-field="role" data-sortable="true" scope="col">Rolle</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php foreach (getUsersBySchoolId($school->getId()) as $user): ?>
-        <?php if (!$user->getStatus()->isReady()): continue; endif; ?>
-        <tr>
-            <td id="td-id-<?= $user->getId() ?>" class="td-class-<?= $user->getId() ?>"
-                data-title="<?= $user->getName() ?>"><?= $user->getName() ?></td>
-            <td><?= $user->getRole()->badge() ?></td>
-        </tr>
-    <?php endforeach; ?>
-    </tbody>
-</table>
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card">
+            <div class="card-header">Nutzer*innen</div>
+            <div class="card-body">
+                <table class="table table-striped table-bordered" data-locale="<?= service('request')->getLocale(); ?>"
+                       data-toggle="table" data-search="true" data-height="500" data-pagination="true"
+                       data-show-columns="true" data-cookie="true" data-cookie-id-table="user"
+                       data-search-highlight="true" data-show-columns-toggle-all="true">
+                    <thead>
+                    <tr>
+                        <th data-field="name" data-sortable="true" scope="col">Vor- und Nachname</th>
+                        <th data-field="role" data-sortable="true" scope="col">Rolle</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php foreach (getUsersBySchoolId($school->getId()) as $user): ?>
+                        <?php if (!$user->getStatus()->isReady()): continue; endif; ?>
+                        <tr>
+                            <td id="td-id-<?= $user->getId() ?>" class="td-class-<?= $user->getId() ?>"
+                                data-title="<?= $user->getName() ?>"><?= $user->getName() ?></td>
+                            <td><?= $user->getRole()->badge() ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
