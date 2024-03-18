8f0941540142ec3208cc9fd09accde439c80870a
elektr0nisch <contact@elektronisch.dev>
2024-03-19 00:52:16 +0100

    Add margin-bottom to last footer item

--- app/Views/components/footer.php
+++ app/Views/components/footer.php
@@ -6,7 +6,7 @@
     <a href="https://waldorfconnect.de/impressum" target="_blank">Impressum</a> &ndash;
     <a href="https://waldorfconnect.de/datenschutz" target="_blank">Datenschutz</a><br>
     <a href="https://instagram.com/waldorfconnect" target="_blank" style="color: black;"><i class="fa-brands fa-instagram fa-2xl me-3 mt-4"></i></a>
-    <a href="https://github.com/WaldorfConnect" target="_blank" style="color: black;"><i class="fa-brands fa-github fa-2xl"></i></a>
+    <a href="https://github.com/WaldorfConnect" target="_blank" style="color: black;"><i class="fa-brands fa-github fa-2xl mb-4"></i></a>
 </footer>
 
 <script src="<?= base_url('/') ?>assets/js/lightbox.min.js"></script>
